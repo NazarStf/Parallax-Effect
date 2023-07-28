362bf578341894c7dbd19869ca09d058f586bd78
NazarStf <hworker98@gmail.com>
2023-07-28 22:58:35 +0300

    Initial Commit

---
+++ Parallax Effect/Parallax_EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Parallax_EffectApp.swift
+//  Parallax Effect
+//
+//  Created by NazarStf on 28.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Parallax_EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
